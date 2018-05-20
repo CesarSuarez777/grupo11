407ba7cd97d405e7232f4467051d411405df9f24
unknown <jlacoste96@gmail.com>
2018-05-20 01:49:31 -0300

    nuevo

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                 <br>
                 <h1><img src="Logo.jpg" alt="imagen no disponible" height="55"><font size="8" face="Univers-Light-Normal">   Aventon</font></h1>
                 <br>
-                <form action="index.php" method="post" class="form-register">
+                <form action="registrar.php" method="post" class="form-register">
                     <h2 class="form_titulo"><font size="6" face="Univers-Light-Normal">Crear una cuenta</font></h2>
                     <div class="contenedor-inputs">
                         <input type="text" name="nombre" placeholder="Nombre" class="input-50" required>
@@ -42,48 +42,6 @@
                         </div>
                     </div>
                 </form>
-                <?php
-                     $link = mysqli_connect("localhost","root","");
-                     if ($link) {
-                         mysqli_select_db($link, "aventon");
-                     }
-                        
-                    $errors = array();
-                    
-                    if(!empty($_POST)){
-                        $nombre = $_POST['nombre'];
-                        $apellido = $_POST['apellidos'];
-                        $email = $_POST['correo'];
-                        $contraseña = $_POST['clave'];
-                        $contraseña2 = $_POST['clave_confirmacion'];
-                        $fecha_nacimiento = $_POST['nacimiento'];
-                        $foto = $_POST['foto_perfil'];
-                        
-                        if($contraseña != $contraseña2){
-                            $errors[] = "Las contraseñas no coinciden";
-                        }
-                        
-                        $usuario_existe = mysqli_prepare($link, "SELECT email FROM usuarios WHERE email = ? ");
-                        $usuario_existe->bind_param("s", $email);
-                        $usuario_existe->execute();           
-                        $usuario_existe->store_result();
-                        $num = $usuario_existe->num_rows;
-                        $usuario_existe->close();
-                        if ($num>0){
-                           $errors[] = "El email ya se encuentra registrado"; 
-                        }    
-                        
-                        if(count($errors) == 0) {
-                           $contraseña_hasheada = password_hash($contraseña, PASSWORD_DEFAULT);
-                           $token = md5(uniqid(mt_rand(),false));
-                           
-                           $stmt = mysqli_prepare($link, "INSERT INTO usuarios VALUES(?,?,?,?,?,?,?)");
-                           $stmt->bind_param('ssdssbs', $nombre, $apellido, $fecha_nacimiento, $contraseña, $email , $foto, $token);
-                           $stmt->execute();
-                           $stmt->close();
-                        }
-                    }
-            ?>
            </div>
         </div>
          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
---
+++ registrar.php
@@ -0,0 +1,27 @@
+ <?php
+                  require 'connect_db.php';
+                    $errors = array();
+                    
+                    if(!empty($_POST)){
+                        $nombre = $_POST['nombre'];
+                        $apellido = $_POST['apellidos'];
+                        $email = $_POST['correo'];
+                        $contraseña = $_POST['clave'];
+                        $contraseña2 = $_POST['clave_confirmacion'];
+                        $fecha_nacimiento = $_POST['nacimiento'];
+                        $foto = $_POST['foto'];
+                        
+                        if($contraseña != $contraseña2){
+                            $errors[] = "Las contraseñas no coinciden";
+                        }
+                        
+                        if (mysqli_query($link, "SELECT nombre FROM usuarios where nombre='$nombre'">0)){
+                           $errors[] = "El email ya se encuentra registrado"; 
+                        }    
+                        
+                        if(count($errors) == 0) {
+                           $token = md5(uniqid(mt_rand(),false));
+                           mysqli_query($link, "INSERT INTO usuarios VALUES('$nombre', '$apellido')");
+                        }
+                    }
+            ?>
