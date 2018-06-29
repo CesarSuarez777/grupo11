8af40cb076d0937032a7c34a320c629667040979
FrancoSpaltro <francospaltro96@gmail.com>
2018-06-29 15:17:19 -0300

    fix asientos

--- cancelarPostulado.php
+++ cancelarPostulado.php
@@ -11,6 +11,8 @@
     
     mysqli_query($link,"UPDATE usuarios SET penalizacion=penalizacion+1 where ID=$id");
     
+    mysqli_query($link, "UPDATE viajes SET asientos_disponibles=asientos_disponibles+1 where IDviaje=$idviaje");
+    
     if(mysqli_query($link, "UPDATE postulados_usuarios_viajes SET estado=-1 where IDviaje=$idviaje AND IDusuario=$iduser")){
             header("Location: verSolicitudes.php?id=$idviaje&exito=true");
     }else{
